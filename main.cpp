de479396e323b79dc227b30a78d935a726123217
richw22jr <richw22@hotmail.com>
2026-09-03 03:40:08 +0000

    Specified namespaces + removed std::

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <string>
 
-// Assignment 1 — Your Name
+// Assignment 1 — Richard Webster
+
+using std::cout;
+using std::cin;
+using std::string;
 
 int main() {
-  std::string name;
+  string name;
   int years = 0;
-  std::cout << "First name: ";
-  std::cin >> name;
-  std::cout << "Years coding goal: ";
-  std::cin >> years;
-  std::cout << name << " — 1/3/5/7 year story goes here (" << years << ")\n";
+  cout << "First name: ";
+  cin >> name;
+  cout << "Years coding goal: ";
+  cin >> years;
+  cout << name << " — 1/3/5/7 year story goes here (" << years << ")\n";
   return 0;
 }
